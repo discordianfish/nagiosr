96ea79732552c27de232d483bd46cf5b10165d2f
Johannes 'fish' Ziemke <github@freigeist.org>
2011-12-01 18:48:53 +0100

    now it works - kind of

--- nagiosr.rb
+++ nagiosr.rb
@@ -5,7 +5,9 @@ require 'sinatra'
 require 'nagios_analyzer'
 require 'json'
 require 'csv'
+require 'facets/hash/slice'
 
+require 'pp'
 STATUS_FILE = '/var/cache/nagios3/status.dat'
 COMMAND_FILE = '/var/lib/nagios3/rw/nagios.cmd'
 
@@ -16,14 +18,19 @@ set :erb, :trim => '-'
 set :bind, '0.0.0.0'
 helpers do
   def get_status(items, selector)
-    puts "doing stuff with #{items}"
+    puts "doing stuff with #{items}, selecting #{selector.pretty_inspect}"
     status = NagiosAnalyzer::Status.new(STATUS_FILE, :include_ok => true)
     unless status.respond_to?(items, params.keys)
       raise Exception, "status.#{items} is invalid"
     end
-  
+ 
     status.send(items).map do |item|
-      item.hash.select {|k,v| selector ? selector.include?(k) : true}
+      begin
+      	item.hash.slice(*selector.map {|s| s.to_sym })
+      rescue IndexError
+	puts "#{selector.pretty_inspect} not found"
+        {}
+      end
     end
   end
 end
@@ -38,9 +45,8 @@ end
 get '/status/?:items?.csv' do
   items = params[:items] || 'items'
   content_type :text
-
-  status = get_status(items, params.keys)
-  header = status.first.keys
+  status = get_status(items, params.keys.reject{|s| s.to_s == 'items'})
+  header = status.first.keys 
   
   [ CSV.generate_line(header), *status.map {|s| CSV.generate_line(s.values)} ].join("\n")
 end
